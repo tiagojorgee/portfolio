fa73e7687fd270cf78248b47aad18ff49b448da0
Tiago Jorge <91836437+tiagojorgee@users.noreply.github.com>
2023-06-27 23:36:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Seu código JavaScript aqui
+    var typed = new Typed(".text", {
+        strings: [ " Engenharia de Software", 
+         "Desenvolvolvimento Back-end", "Análise e comportamento de dados"," Cloud"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Seu código JavaScript aqui
+    var typed = new Typed(".text2", {
+        strings: [ "um futuro Engenheiro de Software!"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
