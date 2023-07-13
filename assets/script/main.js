133f0a2129bc1e4e7e5c1d990ff806b5bbdb4957
Tiago Jorge <tiagosilva305@outlook.com>
2023-07-13 03:12:53 -0300

    subindo...

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Seu código JavaScript aqui
-    var typed = new Typed(".text", {
-        strings: [ " Engenharia de Software", 
-         "Desenvolvolvimento Back-end", "Análise e comportamento de dados"," Cloud"],
-        typeSpeed: 100,
-        backSpeed: 100,
-        backDelay: 1000,
-        loop: true
-    });
-});
 
 document.addEventListener("DOMContentLoaded", function() {
     // Seu código JavaScript aqui
