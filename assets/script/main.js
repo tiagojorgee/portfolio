fff6b92b23cf6437f5ba8cd29bc88fd8293c2396
Tiago Jorge <tiagosilva305@outlook.com>
2023-07-14 14:38:12 -0300

    atualizando...

--- assets/script/main.js
+++ assets/script/main.js
@@ -9,3 +9,23 @@ document.addEventListener("DOMContentLoaded", function() {
         loop: true
     });
 });
+document.addEventListener("DOMContentLoaded", function() {
+    // Seu código JavaScript aqui
+    var typed = new Typed(".text3", {
+        strings: [ "Serviços"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    // Seu código JavaScript aqui
+    var typed = new Typed(".text4", {
+        strings: [ "Skills"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
