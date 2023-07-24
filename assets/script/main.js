0fdaf0e1bafa5b5b5462030b1fb893f61069f0a9
Tiago Jorge <tiagosilva305@outlook.com>
2023-07-24 13:13:59 -0300

    ...

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,8 +1,8 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     // Seu código JavaScript aqui
-    var typed = new Typed(".text2", {
-        strings: [ "um futuro Engenheiro de Software!"],
+    var typed = new Typed(".text", {
+        strings: [ "Engenharia de Software"],
         typeSpeed: 100,
         backSpeed: 100,
         backDelay: 1000,
@@ -23,6 +23,37 @@ document.addEventListener("DOMContentLoaded", function() {
     // Seu código JavaScript aqui
     var typed = new Typed(".text4", {
         strings: [ "Skills"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Seu código JavaScript aqui
+    var typed = new Typed(".text5", {
+        strings: [ " Mim"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    // Seu código JavaScript aqui
+    var typed = new Typed(".text6", {
+        strings: [ "Projetos"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    // Seu código JavaScript aqui
+    var typed = new Typed(".text7", {
+        strings: [ "Contato"],
         typeSpeed: 100,
         backSpeed: 100,
         backDelay: 1000,
