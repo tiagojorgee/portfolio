aac4cd2b9dfdd7c6ff9aed23030fa58e226c45e9
Tiago Jorge <tiagosilva305@outlook.com>
2023-08-28 11:03:39 -0300

    em rede

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // Seu código JavaScript aqui
+    var typed = new Typed(".text",{
+        strings:["Engenharia de Software"],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop: true
+    });
+});
